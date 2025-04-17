e0f52d38eeed59f149a5d43fe5958b934c55570b
Allison Johanson <allymjohanson@gmail.com>
2025-04-16 19:57:00 -0600

    made a gross menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import './App.css';
+import React from 'react';
+import Board from './components/Board';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Board />
     </div>
   );
 }
